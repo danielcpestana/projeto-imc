d03a374a8c6696183ea1a3963fc1927667f8315d
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2024-03-08 14:59:10 -0300

    imc

--- DanHealth/src/components/Form/index.js
+++ DanHealth/src/components/Form/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
-import {View, Text, TextInput, Button} from "react-native"
+import {View, Text, TextInput, TouchableOpacity} from "react-native"
 import ResultImc from "./ResultImc/"
+import styles from "./style"
+
 
 
 export default function Form(){
@@ -27,32 +29,41 @@ function validationImc(){
     }
     setImc(null)
     setTextButton("Calcular")
-    setMessageImc("preencha o pesoa e a altura")
+    setMessageImc("preencha o peso e a altura")
 
 }
 
 
     return(
-        <View>
-            <View>
+        <View style={styles.formContext}>
+            <View style={styles.form}>
 
-
-                <Text>Altura</Text>
+                <Text style={styles.formLabel}>Altura</Text>
                 <TextInput
+                style={styles.input}
                 onChangeText={setHeight}
                 value={height} 
                 placeholder="Ex. 1.75"
                 keyboardType="numeric"
                 ></TextInput>
 
-                <Text>Peso</Text>
+                <Text style={styles.formLabel}>Peso</Text>
                 <TextInput
+                style={styles.input}
                 onChangeText={setWeight}
                 value={weight}
                 placeholder="Ex. 75.365"
                 keyboardType="numeric"
                  ></TextInput>
-                 <Button onPress={() => validationImc()} title={textButton}/>
+                <TouchableOpacity
+                style={styles.ButtonCalculator}
+                onPress={() =>{
+                    validationImc()
+                }}
+                >
+                    <Text style={styles.textbuttonCalculator}>{textButton}</Text>
+                </TouchableOpacity>
+
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc}/>
             
